692f137879d47207b91996a95ae0092e4f9a5391
Samyakk123 <samyak.mehta@mail.utoronto.ca>
2020-12-26 01:16:44 -0500

    Added starter of the header, and color scheme

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,14 @@
 import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Updates right away <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="background"></div>
+      <Header></Header>
     </div>
   );
 }
---
+++ website/src/components/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "./Header.css";
+function header() {
+  return (
+    <>
+      <div className="box">
+        <div id="title">Samyak Mehta</div>
+
+        <div id="subtitles">
+          <div id="subtitle">RESUME</div>
+          <div id="subtitle">ABOUT ME</div>
+          <div id="subtitle">GITHUB</div>
+          <div id="subtitle">LINKEDIN</div>
+        </div>
+      </div>
+      ;
+    </>
+  );
+}
+
+export default header;
