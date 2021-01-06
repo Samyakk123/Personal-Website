bd502cdc071dcd71d39b94a12250e44abb86380e
Samyakk123 <samyak.mehta@mail.utoronto.ca>
2021-01-06 15:39:20 -0500

    Added the project section, need to work on Experience and adding the links. As well as particle.js

--- website/src/components/Main.js
+++ website/src/components/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./Main.css";
 import Header from "./Header";
 import UofTlogo from "../images/Utoronto_coa.svg.png";
+import connect4 from "../images/GamePage.png";
 
 class Main extends Component {
   render() {
@@ -12,7 +13,7 @@ class Main extends Component {
         <div className="centrePiece">
           <div className="introduction">
             <div className="text" id="sub">
-              Welcome to my website!
+              Welcome!
             </div>
 
             <div className="format">
@@ -36,8 +37,133 @@ class Main extends Component {
             </div>
           </div>
 
-          <div className="text">Hello</div>
-          <div className="text">Hello again</div>
+          <div className="projects">
+            <div className="title" id="sub">
+              Projects!
+            </div>
+            <div className="miniSections" id="miniTitle">
+              <div className="projectPicCombo">
+                <h3 className="text2" id="heading">
+                  U-Impactify
+                </h3>
+                <img
+                  src={connect4}
+                  width="200px"
+                  height="150px"
+                  alt="UofTlogo"
+                />
+              </div>
+              <div className="descriptionAndSkills">
+                <ul className="textProject text2">
+                  <li>Player vs player and player vs AI compatibility</li>
+                  <li>
+                    Uses minimax algorithm with alpha beta pruning to determine
+                    ideal moves
+                  </li>
+                  <li>
+                    Various difficulties to accomodate for all different types
+                    of players
+                  </li>
+                </ul>
+
+                <div className="skillSet">
+                  <h3 className="text3" id="heading">
+                    skills
+                  </h3>
+                  <div className="skillHolder">
+                    <div className="skillBox">Java Swing</div>
+                    <div className="skillBox">WindowBuilder</div>
+                    <div className="skillBox">GUI</div>
+                    <div className="skillBox">Component events</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="miniSections" id="miniTitle">
+              <div className="projectPicCombo">
+                <h5 className="text2" id="heading">
+                  Mock Linux Shell
+                </h5>
+                <img
+                  src={connect4}
+                  width="200px"
+                  height="150px"
+                  alt="UofTlogo"
+                />
+              </div>
+              <div className="descriptionAndSkills">
+                <ul className="textProject text2">
+                  <li>Player vs player and player vs AI compatibility</li>
+                  <li>
+                    Uses minimax algorithm with alpha beta pruning to determine
+                    ideal moves
+                  </li>
+                  <li>
+                    Various difficulties to accomodate for all different types
+                    of players
+                  </li>
+                </ul>
+
+                <div className="skillSet">
+                  <h3 className="text3" id="heading">
+                    skills
+                  </h3>
+                  <div className="skillHolder">
+                    <span className="skillBox">Java Swing</span>
+                    <span className="skillBox">WindowBuilder</span>
+                    <span className="skillBox">GUI</span>
+                    <span className="skillBox">Component events</span>
+                  </div>
+                </div>
+
+                <div>More text here!</div>
+              </div>
+            </div>
+
+            <div className="miniSections" id="miniTitle">
+              <div className="projectPicCombo">
+                <h3 className="text2" id="heading">
+                  Connect 4
+                </h3>
+                <img
+                  src={connect4}
+                  width="200px"
+                  height="150px"
+                  alt="UofTlogo"
+                />
+              </div>
+              <div className="descriptionAndSkills">
+                <ul className="textProject text2">
+                  <li>Player vs player and player vs AI compatibility</li>
+                  <li>
+                    Uses minimax algorithm with alpha beta pruning to determine
+                    ideal moves
+                  </li>
+                  <li>
+                    Various difficulties to accomodate for all different types
+                    of players
+                  </li>
+                </ul>
+
+                <div className="skillSet">
+                  <h3 className="text3" id="heading">
+                    skills
+                  </h3>
+                  <div className="skillHolder">
+                    <div className="skillBox">Java Swing</div>
+                    <div className="skillBox">WindowBuilder</div>
+                    <div className="skillBox">GUI</div>
+                    <div className="skillBox">Component events</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="title" id="sub">
+            Experience
+          </div>
         </div>
       </div>
     );
